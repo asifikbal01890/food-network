be7e3a57b3c265376011ae5994d71e492d425ff1
asifilbal01890 <asifikbal01890@gmail.com>
2023-11-06 20:58:25 +0600

    topBar desing complete

--- src/components/NavBar/TopNavBar/TopNavBar.tsx
+++ src/components/NavBar/TopNavBar/TopNavBar.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import logo from '../../../../public/assets/food-network-logo.png';
 import SearchBar from "@/components/ReUseComponents/SearchBar/SearchBar";
+import sunIcon from '../../../../public/assets/sun-01.svg';
+import nintendoIcon from '../../../../public/assets/nintendo-switch.svg';
+import menuOneIcon from '../../../../public/assets/menu-01.svg';
 
 const TopNavBar = () => {
     return (
         <div className="max-w-[1920px] flex justify-between items-center my-[10px] mx-[15px]">
+            {/* left side */}
             <div className="ml-[9px]">
                 <Image
                  src={logo}
@@ -13,11 +17,38 @@ const TopNavBar = () => {
                   height="38"
                 />
             </div> 
+
+            {/* middle component */}
             <div>
-<SearchBar></SearchBar>
+                <SearchBar></SearchBar>
             </div>
-            <div>
-hello
+
+            {/* right side */}
+            <div className="flex gap-6">
+               <button className="border-2 border-[#E655271A] bg-[#E655271A] p-[6px] rounded-[6px]">
+                  <Image
+                     src={sunIcon}
+                     alt="sunIcon"
+                     width="20"
+                     height="20"
+                  />
+                </button>
+               <button className="border-2 border-[#E655271A] bg-[#E655271A] p-[6px] rounded-[6px]">
+                  <Image
+                     src={nintendoIcon}
+                     alt="nintendoIcon"
+                     width="20"
+                     height="20"
+                  />
+                </button>
+               <button className="border-2 border-[#1010101A] bg-[#1010101A] p-[6px] rounded-[6px]">
+                  <Image
+                     src={menuOneIcon}
+                     alt="menuOneIcon"
+                     width="20"
+                     height="20"
+                  />
+                </button>
             </div>
         </div>
     );
